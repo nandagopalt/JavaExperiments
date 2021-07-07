d70c112eff5e61a4eeea2a0d6aa876e81a5ef30c
Nandagopal <gopikrish.thiru@gmail.com>
2021-07-07 22:35:24 +0530

    InputExample

---
+++ src/com/example/hankerrankexample/InputExample1.java
@@ -0,0 +1,17 @@
+package com.example.hankerrankexample;
+
+import java.util.Scanner;
+
+public class InputExample1 {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		scanner.useDelimiter(";");
+		int count = 0;
+		while(scanner.hasNext()) {
+			count++;
+			String input = scanner.next();
+			System.out.println(String.format("%d %s", count,input));
+		}
+		scanner.close();
+	}
+}
